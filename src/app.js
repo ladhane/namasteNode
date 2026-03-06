95799aea1ea2afb4fec4b266573b89808ea1e792
Mayuri Ladhane <mayuriladhane3@gmail.com>
2026-03-06 07:24:27 +0530

    S02:Middleware and schema validation

--- src/app.js
+++ src/app.js
@@ -3,19 +3,78 @@ const connectDB = require('./config/database');
 const User = require('./models/user')
 const server = express();
 
+server.use(express.json());
+//APT to signup
 server.post('/signup', async(req,res)=>{
-    const user = new User({
-        firstName: 'Mayuri',
-        lastName: 'Ladhane',
-        email: 'mayuriladhane3@gmail.com',
-        password: 'Mayuri123'
-    })
-
+    const user = new User(req.body);
     try {
         await user.save();
         res.send('Data saved successfully');
     }catch (err){
+        res.status(400).send('Error Message:' + err.message)
+    }
+})
+
+//API to GET a user
+server.get('/user',async(req,res)=>{
+    try{
+        const emailId = req.body.email;
+        const user = await User.findOne({email:emailId});
+        if(!user){
+             res.send('User not found');
+        }else{
+             res.send(user);
+        }
+    }catch(err){
+        res.status(400).send('Something went wrong!')
+    }
+})
+
+//API to GET all the users
+server.get('/feed',async(req,res)=>{
+    try{
+        const users = await User.find({});
+        console.log(users);
+        console.log("length",users.length);
+        if(users.length === 0){
+             res.send('User not found');
+        }else{
+             res.send(users);
+        }
+    }catch(err){
+        res.status(400).send('Something went wrong!')
+    }
+})
+
+//API to DELETE a user 
+server.delete('/user',async(req,res)=>{
+    try{
+        const userId = req.body.userId
+        // const user = await User.findOneAndDelete({_id:userId})
+        //Other way of doing it
+        const user = await User.findByIdAndDelete(userId)
+        res.send(user)
+    } catch(err){
         res.status(400).send('Something went wrong')
+    }
+})
+
+//API to UPDATE a user
+server.patch('/user',async(req,res)=>{
+    try{
+        // const userId = req.body.userId;
+        // 3rd param here is options where we can pass multiple things like new with value as
+        //'false' -> to return data before update
+        //'true' -> to return data after update
+        // false is default value
+        // We have other options like sort,etc. refer documentation to know about them
+        const user = await User.findByIdAndUpdate(userId, req.body);
+
+        //with findOneAndUpdate we can use any parameter to find the document instead of ID
+        // const user = await User.findOneAndUpdate({email:req.body.emailId},req.body,{ returnDocument: 'after', runValidators: true});
+        res.send(user);
+    }catch(err){
+        res.status(400).send('Update Error Message : ' + err.message)
     }
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,61 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
     firstName:{
         type: String,
+        required: true,
+        maxLength: 20
     },
     lastName:{
         type: String,
+        maxLength:20
     },
     email:{
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,'Email id is not valid']
     },
     password:{
-        type: String
+        type: String,
+        required: true,
+        validate(value) {
+            const regex =
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+            if (!regex.test(value)) {
+                throw new Error('Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one digit, and one special character.');
+            }
+        }
     },
     gender:{
-        type: String
+        type: String,
+        validate(value){
+            if(!['M','F','O'].includes(value)){
+                throw new Error('Your Gender must be M, F or O');
+            }
+        }
+    },
+    photoUrl:{
+        type: String,
+        default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
     },
     age:{
-        type: Number
+        type: Number,
+        min: 18
+    },
+    about:{
+        type: String,
+        maxLength: 250,
+        default: 'Hey there! I am on DevSocial.'
+    }, 
+    skills:{
+        type: [String],
     }
+},
+{
+    timestamps: true
 });
 
 const User = mongoose.model('User', userSchema);
